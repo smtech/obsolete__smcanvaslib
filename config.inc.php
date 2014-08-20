211fed0e0833c920735e70f7b0df945cab843d3f
Seth Battis <SethBattis@stmarksschool.org>
2014-08-20 09:28:38 -0400

    HTTPS
    Set the protocol to for the APP_URL (and therefore also SMCANVASLIB_URL) to be the same as the request protocol.
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- config.inc.php
+++ config.inc.php
@@ -2,7 +2,7 @@
 
 /* this will break if there are mod_rewrites, but will do for now... */
 if(!isset($argc)) {
-	define('APP_URL', 'http://' . $_SERVER['SERVER_NAME'] . preg_replace('|(.*)/' . basename(__DIR__) . '|', '$1', str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', realpath(__DIR__))));
+	define('APP_URL', (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['SERVER_NAME'] . preg_replace('|(.*)/' . basename(__DIR__) . '|', '$1', str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', realpath(__DIR__))));
 	define('SMCANVASLIB_URL', APP_URL . '/smcanvaslib');
 }
 define('APP_PATH', preg_replace('|(.*)/' . basename(__DIR__) . '|', '$1', realpath(__DIR__)));
